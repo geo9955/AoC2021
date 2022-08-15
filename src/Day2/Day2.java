7e65b012ea0afe9b6e0c5070d62c1434b2ed03cf
George Mayrink <georgemayrink@yahoo.com>
2022-08-14 23:48:57 -0700

    Implement Day 2 Part 2

--- src/Day2/Day2.java
+++ src/Day2/Day2.java
@@ -50,6 +50,45 @@ public class Day2 {
     }
 
     public static void part2() {
+        File file = new File("src\\Day2\\input.txt");
+        Scanner reader;
 
+        try {
+            reader = new Scanner(file);
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found. Aborting");
+            return;
+        }
+
+        int hpos = 0, depth = 0, aim = 0, x;
+        String[] inputs;
+        String line, dir;
+
+        while(reader.hasNextLine()) {
+            line = reader.nextLine();
+            inputs = line.split(" ");
+            dir = inputs[0];
+            x = Integer.parseInt(inputs[1]);
+            
+            switch(dir) {
+                case "forward":
+                hpos += x;
+                depth += aim * x;
+                break;
+
+                case "down":
+                aim += x;
+                break;
+
+                case "up":
+                aim -= x;
+                break;
+            }
+        }
+
+        System.out.println("Horizontal Position: " + hpos + " Depth: " + depth);
+        System.out.println("Both: " + hpos * depth);
+
+        reader.close();
     }
 }
