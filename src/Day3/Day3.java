5597f98b89d13de581ac8f324dc9df84f2989115
George Mayrink <georgemayrink@yahoo.com>
2022-08-16 19:04:21 -0500

    Implement Day 3 Part 2

--- src/Day3/Day3.java
+++ src/Day3/Day3.java
@@ -64,6 +64,72 @@ public class Day3 {
     }
     
     public static void part2() {
-        
+        File file = new File("src\\Day3\\input.txt");
+        Scanner reader;
+
+        try {
+            reader = new Scanner(file);
+        } catch(FileNotFoundException e) {
+            System.out.println("File not found. Aborting");
+            return;
+        }
+
+        ArrayList<String> oxygenNums = new ArrayList<String>();
+        while(reader.hasNextLine()) {
+            oxygenNums.add(reader.nextLine());
+        }
+
+        ArrayList<String> co2Nums = (ArrayList<String>) oxygenNums.clone();
+        int bitNum = 0, zeros = 0, ones = 0;
+        char bitCriteria;
+
+        while(oxygenNums.size() > 1) {
+            for(String number : oxygenNums) {
+                if(number.charAt(bitNum) == '0')
+                    zeros++;
+                else
+                    ones++;
+            }
+
+            if(zeros > ones)
+                bitCriteria = '0';
+            else
+                bitCriteria = '1';
+
+            int finalBitNum = bitNum;
+            char finalBitCriteria = bitCriteria;
+            oxygenNums.removeIf(s -> s.charAt(finalBitNum) != finalBitCriteria);
+
+            bitNum++;
+            zeros = ones = 0;
+        }
+
+        bitNum = 0;
+
+        while(co2Nums.size() > 1) {
+            for(String number : co2Nums) {
+                if(number.charAt(bitNum) == '0')
+                    zeros++;
+                else
+                    ones++;
+            }
+
+            if(ones < zeros)
+                bitCriteria = '1';
+            else
+                bitCriteria = '0';
+
+            int finalBitNum1 = bitNum;
+            char finalBitCriteria1 = bitCriteria;
+            co2Nums.removeIf(s -> s.charAt(finalBitNum1) != finalBitCriteria1);
+            bitNum++;
+            zeros = ones = 0;
+        }
+
+        int oxyRating = Integer.parseInt(oxygenNums.get(0), 2);
+        int co2Rating = Integer.parseInt(co2Nums.get(0), 2);
+
+        System.out.println("Oxygen Rating" + oxyRating + " CO2 Rating: " + co2Rating);
+        System.out.println("Life Support Rating: " + oxyRating * co2Rating);
     }
 }
