a954eb61549bd62d95c88f24cbf5236bc45f4652
George Mayrink <georgemayrink@yahoo.com>
2022-08-18 22:49:34 -0500

    Polish Day 4

--- src/Day4/Day4.java
+++ src/Day4/Day4.java
@@ -2,18 +2,17 @@ package Day4;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Day4 {
 
-    final static int BOARD_HEIGHT = 5;
-    final static int BOARD_WIDTH = 5;
+    private static File file = new File("src\\Day4\\input.txt");
+    private static Scanner reader;
 
+    /*
+    Finds the board that wins first and calculates its score, printing it to the console.
+     */
     public static void part1() {
-        File file = new File("src\\Day4\\input.txt");
-        Scanner reader;
         try {
             reader = new Scanner(file);
         } catch(FileNotFoundException e) {
@@ -24,7 +23,7 @@ public class Day4 {
         String numberInput = reader.nextLine();
         int[] numbers = Arrays.stream(numberInput.split(",")).mapToInt(Integer::parseInt).toArray();
 
-        ArrayList<Board> boards = createBoards(reader);
+        ArrayList<BingoCard> cards = createCards(reader);
 
         boolean winnerFound = false;
 
@@ -32,11 +31,11 @@ public class Day4 {
             if(winnerFound)
                 break;
 
-            for(Board board : boards) {
-                board.markNumber(number);
-                if(board.checkForWin()) {
-                    System.out.println("Board number " + board.boardNumber + " will win first.");
-                    System.out.println("Its score is " + board.calculateScore(number) + " points.");
+            for(BingoCard card : cards) {
+                card.stampNumber(number);
+                if(card.checkForWin()) {
+                    System.out.println("Card number " + card.cardNumber + " will win first.");
+                    System.out.println("Its score is " + card.calculateScore(number) + " points.");
                     winnerFound = true;
                     break;
                 }
@@ -46,10 +45,10 @@ public class Day4 {
         reader.close();
     }
 
+    /*
+    Finds the board that wins *last* and calculates its score, printing it to the console
+     */
     public static void part2() {
-
-        File file = new File("src\\Day4\\input.txt");
-        Scanner reader;
         try {
             reader = new Scanner(file);
         } catch(FileNotFoundException e) {
@@ -60,7 +59,7 @@ public class Day4 {
         String numberInput = reader.nextLine();
         int[] numbers = Arrays.stream(numberInput.split(",")).mapToInt(Integer::parseInt).toArray();
 
-        ArrayList<Board> boards = createBoards(reader);
+        ArrayList<BingoCard> cards = createCards(reader);
         int numOfWinners = 0;
         boolean loserFound = false;
 
@@ -68,18 +67,18 @@ public class Day4 {
             if(loserFound)
                 break;
 
-            for(Board board : boards) {
-                if(board.hasWon)
+            for(BingoCard card : cards) {
+                if(card.hasWon)
                     continue;
 
-                board.markNumber(number);
-                if(board.checkForWin()) {
-                    board.hasWon = true;
+                card.stampNumber(number);
+                if(card.checkForWin()) {
+                    card.hasWon = true;
                     numOfWinners++;
-                    if(numOfWinners == boards.size()) {
+                    if(numOfWinners == cards.size()) {
                         loserFound = true;
-                        System.out.println("The last winner is board " + board.boardNumber + ".");
-                        System.out.println("Its score is " + board.calculateScore(number));
+                        System.out.println("The last winner is card " + card.cardNumber + ".");
+                        System.out.println("Its score is " + card.calculateScore(number) + ".");
                         break;
                     }
                 }
@@ -89,102 +88,116 @@ public class Day4 {
         reader.close();
     }
 
-    private static ArrayList<Board> createBoards(Scanner scanner) {
-        ArrayList<Board> boards = new ArrayList<>();
+    /*
+    Returns an ArrayList of Boards created from the given input
+     */
+    private static ArrayList<BingoCard> createCards(Scanner scanner) {
+        ArrayList<BingoCard> cards = new ArrayList<>();
         int boardNumber = 0;
 
         while(scanner.hasNextInt()) {
-            boards.add(new Board(scanner, boardNumber));
+            cards.add(new BingoCard(scanner, boardNumber));
             boardNumber++;
         }
 
-        return boards;
+        return cards;
+    }
+}
+
+/*
+ Represents a Bingo card that can be stamped as numbers are called. Numbers are stored in a HashMap for quick access,
+ as well as incremental aliases for those numbers to aid in row/column checking.
+ */
+class BingoCard {
+
+    private final int BOARD_HEIGHT = 5;
+    private final int BOARD_WIDTH = 5;
+    private Map<Integer, BingoNumber> numbers = new HashMap<>();
+    private Map<Integer, Integer> aliases = new HashMap<>();
+    int cardNumber;
+    boolean hasWon = false;
+
+    public BingoCard(Scanner scanner, int cardNumber) {
+        for(int i = 0; i < BOARD_HEIGHT * BOARD_WIDTH; i++) {
+            int value = scanner.nextInt();
+            numbers.put(value, new BingoNumber(value));
+            aliases.put(i, value);
+        }
+
+        this.cardNumber = cardNumber;
     }
 
+    /*
+    Stamps a number if it is present on the Board
+     */
+    public void stampNumber(int number) {
+        BingoNumber temp = numbers.get(number);
 
-    private static class Board {
-
-        class BingoNumber {
-            int number;
-            boolean called;
-            public BingoNumber(int number) {
-                this.number = number;
-                this.called = false;
-            }
+        if(temp != null) {
+            temp.stamped = true;
         }
+    }
 
-        BingoNumber[][] numbers = new BingoNumber[BOARD_HEIGHT][BOARD_WIDTH];
-        int boardNumber;
-        boolean hasWon;
+    public boolean checkForWin() {
+        return checkCols() || checkRows();
+    }
 
-        public Board(Scanner scanner, int boardNumber) {
-            for(int i = 0; i < BOARD_HEIGHT; i++) {
-                for(int j = 0; j < BOARD_WIDTH; j++) {
-                    numbers[i][j] = new BingoNumber(scanner.nextInt());
-                }
-            }
-
-            this.boardNumber = boardNumber;
-            this.hasWon = false;
-        }
-
-        public void markNumber(int number) {
-            for(int row = 0; row < BOARD_HEIGHT; row++) {
-                for(int col = 0; col < BOARD_WIDTH; col++) {
-                    if(numbers[row][col].number == number) {
-                        numbers[row][col].called = true;
-                        return;
-                    }
-                }
-            }
-        }
-
-        public boolean checkForWin() {
-            return checkCols() || checkRows();
-        }
-
-        public int calculateScore(int numCalled) {
-            int sumUncalled = 0;
-
-            for(int row = 0; row < BOARD_HEIGHT; row++) {
-                for(int col = 0; col < BOARD_WIDTH; col++)
-                    if(!numbers[row][col].called)
-                        sumUncalled += numbers[row][col].number;
-            }
-
-            return sumUncalled * numCalled;
-        }
-
-        private boolean checkRows() {
-            int numsCalled;
-            for(int row = 0; row < BOARD_HEIGHT; row++) {
-                numsCalled = 0;
-                for(int col = 0; col < BOARD_WIDTH; col++) {
-                    if(numbers[row][col].called)
-                        numsCalled++;
-                }
-
+    /*
+    Returns true if every number in a row is stamped, and false otherwise
+     */
+    private boolean checkRows() {
+        for(int i = 0; i < BOARD_WIDTH * BOARD_HEIGHT; i += BOARD_WIDTH) {
+            int numsCalled = 0;
+            for(int j = i; j < BOARD_WIDTH + i; j++) {
+                if(numbers.get(aliases.get(j)).stamped)
+                    numsCalled++;
                 if(numsCalled == BOARD_WIDTH)
                     return true;
             }
-
-            return false;
         }
 
-        private boolean checkCols() {
-            int numsCalled;
-            for(int col = 0; col < BOARD_WIDTH; col++) {
-                numsCalled = 0;
-                for(int row = 0; row < BOARD_HEIGHT; row++) {
-                    if(numbers[row][col].called)
-                        numsCalled++;
-                }
+        return false;
+    }
 
+    /*
+    Returns true if every number in a column is stamped, and false otherwise
+     */
+    private boolean checkCols() {
+        for(int i = 0; i < BOARD_WIDTH; i++) {
+            int numsCalled = 0;
+            for(int j = i; j < BOARD_WIDTH * BOARD_HEIGHT; j += BOARD_WIDTH) {
+                if(numbers.get(aliases.get(j)).stamped)
+                        numsCalled++;
                 if(numsCalled == BOARD_HEIGHT)
                     return true;
             }
+        }
 
-            return false;
+        return false;
+    }
+
+    /*
+    Returns the sum of all uncalled numbers on the board, times the last number called
+     */
+    public int calculateScore(int numCalled) {
+        int sumUncalled = 0;
+
+        for(BingoNumber number : numbers.values())
+            if(!number.stamped)
+                sumUncalled += number.value;
+
+        return sumUncalled * numCalled;
+    }
+
+    /*
+    Represents an individual number on a Bingo card
+     */
+     private static class BingoNumber {
+        int value;
+        boolean stamped;
+        public BingoNumber(int number) {
+            this.value = number;
+            this.stamped = false;
         }
     }
 }
